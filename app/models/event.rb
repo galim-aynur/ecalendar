b7a9854fe2d75e1f27399316e7fd3ac73a7004f3
Aynur Galimov <galim-aynur@ya.ru>
2019-10-23 13:32:16 +0500

    Add func for serialize recurring to hash

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,13 @@
 class Event < ApplicationRecord
+  serialize :recurring, Hash
+
+  def recurring=(value)
+    if value = 'null'
+      super(nil)
+    elsif RecurringSelect.is_valid_rule?(value)
+      super(RecurringSelect.dirty_hash_to_rule(value).to_hash)
+    else
+      super(nil)
+    end
+  end
 end
